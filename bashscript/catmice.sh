97efadda00d834748ae12f41093453d54848f7f7
Lizzy <lizzy@localhost.localdomain>
2020-04-08 14:17:17 +0100

    totals added to cat and mice

--- bashscript/catmice.sh
+++ bashscript/catmice.sh
@@ -3,13 +3,21 @@
 
 # numbers script
 
+c=0
+c1=0
+c2=0
+
 for num in {0..100} ; do
-        if  (! ((num % 3)) ) && (! ((num % 5)) ); then echo "Cat and Mouse"
-	elif (! ((num % 3)) ); then echo "Cat"
-	elif (! ((num % 5)) ); then echo "Mouse"
+        if  (! ((num % 3)) ) && (! ((num % 5)) ); then echo "Cat and Mouse" && c=$(($c+1))
+	elif (! ((num % 3)) ); then echo "Cat" && c1=$(($c1+1))
+	elif (! ((num % 5)) ); then echo "Mouse" && c2=$(($c2+1))
 	else echo $num;
 fi
 
-
 done
+
+echo "The total number of Cat and Mouse is $c!"
+echo "The total number of Cat's is $c1!"
+echo "The total number of Mice is $c2!"
+
 
